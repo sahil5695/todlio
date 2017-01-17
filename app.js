b6765df444e3acdf5bac9d62355e74dbe8852417
Sahil Arora <sahilarora@Sahils-MacBook-Air.local>
2017-01-17 17:05:13 +0530

    cors

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (err, req, res, next) {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", ['GET', 'POST', 'PUT', 'DELETE']);
+  res.header("Access-Control-Allow-Methods", ['GET', 'POST', 'PUT', 'OPTIONS']);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
